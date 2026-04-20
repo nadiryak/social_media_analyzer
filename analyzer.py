55c9c2270101224a7233405541b11883b551cf37
Nadir <yakoubinadir74@gmail.com>
2026-04-20 11:56:21 +0100

    Refactor social network analysis functions and fixing bugs.
    Fixing bugs in cycle detection function and commenting the dfs function meaning for search  by depth or (depth first search)

--- analyzer.py
+++ analyzer.py
@@ -8,7 +8,7 @@ RESEAU = [
 print("!!!!!!!!!!!!!!!! ON COMMENCE PAR L'ANALYSE D'UN RESEAU SOCIAL !!!!!!!!!!!!!!!!!!!!!!!!!")
 g=input("POUR COMMENCER APPUIE SUR N'IMPORTE QUEL TOUCHE ")
 
-def nbr_de_edges(reseau):
+def nbr_de_edges(reseau):  # on compte tous les arcs du graphe en additionnant la taille de chaque liste
     cpt=0
     for res in reseau:
         cpt+=len(res)
@@ -18,22 +18,27 @@ def nbr_de_edges(reseau):
 
 def nbr_de_followers(pers,reseau):
     if pers>=len(reseau):
+         # on vérifie que la personne existe dans le réseau
         return "ERROR: votre personne n'existe pas dans notre liste (INDEX OUT OF RANGE)"
     else:
         nb=0
         for p in reseau:
+             # si "pers" apparaît dans la liste de suivis de p
+             # ça veut dire que p suit "pers" -> p est un follower de "pers"
             if pers in p :
                 nb+=1
         return nb
 
 
 
-def la_personne_la_plus_suivie(reseau):
+def la_personne_la_plus_suivie(reseau):        
+    # on construit la liste du nombre de followers pour chaque personne
     liste_nbr_follower= []
     i=0
     for pers in reseau :
         liste_nbr_follower.append(nbr_de_followers(i,reseau))
         i+=1
+    # puis on cherche l'index du maximum = la personne la plus suivie
     maximum=max(liste_nbr_follower)    
     personne=liste_nbr_follower.index(max(liste_nbr_follower))
     print(f"La personne la plus suivie par les gense est {personne} avec {maximum} abonnées. ")
@@ -44,10 +49,10 @@ def la_personne_qui_suit_plus_de_personne(reseau):
     if not reseau:
         return "Votre liste de reseau est vide "
     else:
-        maximum =len(reseau[0])
+        maximum =len(reseau[0]) # on initialise avec la première personne comme référence
         personne=0
         i=0
-        for pers in reseau:
+        for pers in reseau:    # on parcourt pour trouver celle qui a la liste la plus longue
             
             if len(pers)>maximum:
                 maximum=len(pers)
@@ -65,20 +70,9 @@ def la_personne_qui_suit_plus_de_personne(reseau):
 #         maximum = len(pers)
 #         personne = i
         return s
-    
 
 
-
-def cycle_detecte(reseau):
-    for i in range(reseau):
-        for pers in reseau[i]:
-            if i in pers:
-                detect=True
-                break
-            else:
-                None
-
-    
+#CETTE FONCTION A ETE APPRISE POUR DES RAISONS D'APPRENTISSAGE ET PAR CURIOSITE POUR POOUVOIR DETECTER LES CYCLE DANS LE PROGRAME D'APRES    
 def dfs(reseau, start, visited=None):
     if visited is None:
         visited = set()  # ensemble des personnes déjà visitées
@@ -88,9 +82,31 @@ def dfs(reseau, start, visited=None):
 
     for voisin in reseau[start]:  # explore toutes les personnes suivies
         if voisin not in visited:
-            dfs(reseau, voisin, visited) 
-        else: 
-            return True
-        
-
+            dfs(reseau, voisin, visited)  
     return visited
+
+
+def cycle_detecte(reseau, start, visited=None, en_cours=None):
+     if visited is None:          #initaliser visited et en_cours si elles sont vide 
+        visited = set()
+    if en_cours is None:
+        en_cours = set()
+    
+    visited.add(start)   # on marque le nœud comme visité et comme "en cours de visite sur ce chemin"
+    en_cours.add(start)    
+   for suivie in reseau[start]:
+       
+       if suivie in en_cours:  # si on retombe sur un nœud qu'on est en train de visiter 
+           return True  # Cycle détecté
+      # si la personne suivie n'a pas encore été visité, on plonge dedans
+      # si l'appel récursif détecte un cycle, on return True 
+       if suivie not in visited:
+           if cycle_detecte(reseau,suivie,visited,en_cours):
+               return True
+               
+   # on repart en arrière,ce nœud n'est plus sur le chemin actuel           
+   en_cours.remove(start)
+   return False
+
+    
+
